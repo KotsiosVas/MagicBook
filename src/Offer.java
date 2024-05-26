1d474f2f4a55da14b9d6c3fe261c663ab1135ffd
KotsiosVas <konstantinosvas4@gmail.com>
2024-05-26 20:30:05 +0300

    Delete src directory

--- src/Offer.java
+++
@@ -1,9 +0,0 @@
-package MagicBook;
-public class Offer extends Products {
-    private int Product_id;
-    private String Product_Name;
-    private int Product_Amount;
-    private String Product_Description;
-    private double Product_Price;
-    private double Product_New_Price;
-}
--- src/Premium_User.java
+++
@@ -1,7 +0,0 @@
-package MagicBook;
-class Premium_User extends User {
-    public boolean premimum=true;
-    public Premium_User(){
-        super();
-    }
-}
